a7eb15e13cee277c2bf799e2479c4273fd371097
Sharanu <malipatils249@gmail.com>
2025-11-09 23:01:56 +0530

    🎯 Day 1 – Base Spring Boot setup with MySQL, structure & open security

---
+++ demo/src/main/java/com/example/demo/SmartStudyPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStudyPlatformApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStudyPlatformApplication.class, args);
+		System.out.println("Smart Study Platform backend started...");
+	}
+}
